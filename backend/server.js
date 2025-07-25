b10b63f97e27000749f7ffd6f23761a43030c68c
jimmypuli <armg296@gmail.com>
2025-07-25 13:06:50 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
     origin: [
         //'http://localhost:5173', // Development
-        'https://job-reviews-main.vercel.app/' // Your Vercel URL
+        'https://job-reviews-main.vercel.app' // Your Vercel URL
     ],
     credentials: true
 }));
